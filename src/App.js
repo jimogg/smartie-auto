4495fb4da8c2ac0a7a6e45f36f0097b8a85d9fa5
jimogg <jnogana@gmail.com>
2021-05-22 09:32:19 -0400

    More work with json data.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Nav from "./components/Nav"
+import Nav from "./components/Nav";
+import Results from "./components/Results";
 
 function App() {
   return (
@@ -7,9 +8,9 @@ function App() {
       <div className="header"><h1>Smartie Automotive Information</h1></div>
       <nav className="nav"><Nav /></nav>
       <div className="left">Left</div>
-      <div className="main"><h3>Main</h3>
-        <p>Officia proident ea fugiat magna veniam magna deserunt irure. Aliqua anim non duis sunt dolor nulla tempor id ad. Culpa ea nulla adipisicing irure proident magna aute. Qui nisi aliqua non sint labore Lorem deserunt deserunt eiusmod laborum. In consectetur enim exercitation velit elit ad excepteur ex. </p>
-        <p>Do ad commodo sint reprehenderit incididunt cupidatat reprehenderit reprehenderit ut est. Commodo sunt dolore deserunt voluptate. Id tempor aliqua nisi qui labore sunt quis anim. Nisi ullamco veniam exercitation incididunt sit laboris. Reprehenderit veniam aute voluptate minim.</p>
+      <div className="main">
+        <Results />
+
       </div>
       {/* <div className="right">Right</div> */}
       <div className="footer">Footer</div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 const Home = () => {
     return (
         <div>
-            This is the HOME component.
+            <h2>This is the HOME component.</h2>
+            <p>Officia proident ea fugiat magna veniam magna deserunt irure. Aliqua anim non duis sunt dolor nulla tempor id ad. Culpa ea nulla adipisicing irure proident magna aute. Qui nisi aliqua non sint labore Lorem deserunt deserunt eiusmod laborum. In consectetur enim exercitation velit elit ad excepteur ex. </p>
+            <p>Do ad commodo sint reprehenderit incididunt cupidatat reprehenderit reprehenderit ut est. Commodo sunt dolore deserunt voluptate. Id tempor aliqua nisi qui labore sunt quis anim. Nisi ullamco veniam exercitation incididunt sit laboris. Reprehenderit veniam aute voluptate minim.</p>
         </div>
     );
 };
