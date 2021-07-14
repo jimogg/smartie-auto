7d80568c715372108621c75db9ae4e928ccd0610
jimogg <jnogana@gmail.com>
2021-07-13 22:21:42 -0400

    create static file of api models

---
+++ src/models-by-make-modelYear.js
@@ -0,0 +1,61 @@
+{
+    "count": 14,
+        "message": "Results returned successfully",
+            "results": [{
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "BEETLE"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "BEETLE CONVERTIBLE"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "CC"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "E-GOLF"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "EOS"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "GOLF"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "GOLF SPORTWAGEN"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "GTI"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "JETTA"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "JETTA HYBRID"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "PASSAT"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "TIGUAN"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "TOUAREG"
+            }, {
+                "modelYear": "2015",
+                "make": "VOLKSWAGEN",
+                "model": "TOUAREG HYBRID"
+            }]
+}
